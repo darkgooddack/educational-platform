b9d3da72eb0b2187f303c647422fb88273837449
Mike <34812500+mikey-semy@users.noreply.github.com>
2025-01-14 08:42:39 +0000

    fix: правка импортов роутера authentication (зря я импользую не сокращение)

--- api_gateway/app/routes/v1/authentication/__init__.py
+++ api_gateway/app/routes/v1/authentication/__init__.py
@@ -1,2 +1,14 @@
+"""
+Общий роутер для аутентификации
+
+"""
+from fastapi import APIRouter
+
 from .endpoints import router as auth_router
 from .oauth import router as oauth_router
+
+router = APIRouter()
+router.include_router(auth_router)
+router.include_router(oauth_router)
+
+__all__ = ["router"]
