e31012ce628e211d142b67f19f61d8cd20f2a7c6
Mike <34812500+mikey-semy@users.noreply.github.com>
2025-01-14 08:29:53 +0000

    fix: правка импортов схем api_gateway

--- api_gateway/app/schemas/__init__.py
+++ api_gateway/app/schemas/__init__.py
@@ -4,18 +4,21 @@
 Предоставляет единую точку доступа ко всем Pydantic схемам.
 """
 
-from .v1.base import BaseInputSchema, BaseSchema
+from .v1.base import BaseInputSchema, BaseSchema, CommonBaseSchema
 from .v1.cache import RouteCacheSchema, TokenCacheSchema
 from .v1.authentication import AuthenticationSchema, TokenSchema, OAuthResponse
-from .v1.registration import RegistrationSchema
+from .v1.registration import RegistrationSchema, RegistrationResponseSchema
 
 __all__ = [
     "BaseSchema",
     "BaseInputSchema",
+    "CommonBaseSchema",
+    "AuthenticationSchema",
+    "TokenSchema",
     "OAuthResponse",
     "TokenCacheSchema",
     "RouteCacheSchema",
-    "AuthenticationSchema",
-    "TokenSchema",
-    "RegistrationSchema"
+    "RegistrationSchema",
+    "RegistrationResponseSchema",
+
 ]
