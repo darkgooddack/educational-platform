b72709489fbc8f5eeb1e34f2b036273d93233d21
Mike <34812500+mikey-semy@users.noreply.github.com>
2025-01-21 17:11:36 +0300

    fix: TokenSchema

--- app/schemas/v1/auth/auth.py
+++ app/schemas/v1/auth/auth.py
@@ -2,6 +2,7 @@
 Схемы для аутентификации и управления пользователями.
 """
 
+from typing import Optional
 from pydantic import EmailStr, Field
 
 from ..base import BaseInputSchema
@@ -30,7 +31,11 @@ class TokenSchema(BaseInputSchema):
     Args:
         access_token (str): Токен доступа.
         token_type (str): Тип токена.
+        provider: (str) Провайдер.
+        email: (str): Email.
     """
 
     access_token: str
-    token_type: str
+    token_type: str = "bearer"
+    provider: Optional[str] = None
+    email: Optional[str] = None
