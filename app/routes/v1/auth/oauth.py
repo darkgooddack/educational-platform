9718c8c52838a37109d262c60b350236c997cd37
MikhailTo <toshkin.mikhail@yandex.ru>
2025-01-24 17:24:06 +0300

    debug: с get  на post пробуем как с обычной auth

--- app/routes/v1/auth/oauth.py
+++ app/routes/v1/auth/oauth.py
@@ -43,7 +43,7 @@ def setup_routes(router: APIRouter):
         """
         return await OAuthService(db_session).get_oauth_url(provider)
 
-    @router.get("/{provider}/callback")
+    @router.post("/{provider}/callback")
     async def oauth_callback(
         provider: str,
         code: str,
