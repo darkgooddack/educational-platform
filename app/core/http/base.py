8e38054b8fe49735dd5c2a1478729d089095ee31
MikhailTo <toshkin.mikhail@yandex.ru>
2025-02-05 23:03:28 +0300

    fix: фильтрация Null значений для aiohttp

--- app/core/http/base.py
+++ app/core/http/base.py
@@ -18,6 +18,10 @@ class BaseHttpClient:
             return await resp.json()
 
     async def post(self, url: str, data: dict = None, headers: dict = None) -> dict:
+        if data:
+            # Фильтруем None значения из параметров
+            data = {k: v for k, v in data.items() if v is not None}
+
         session = await self._get_session()
         async with session.post(url, data=data, headers=headers) as resp:
             return await resp.json()
