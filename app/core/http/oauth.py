7f3e006b6e597c707ef6f33cb5d3692bf3982183
mikey-semy <toshkin.mikhail@yandex.ru>
2025-02-06 07:23:56 +0000

    fix: ловим ContentTypeError

--- app/core/http/oauth.py
+++ app/core/http/oauth.py
@@ -1,16 +1,19 @@
 from .base import BaseHttpClient
-
+from aiohttp.client_exceptions import ContentTypeError
 
 class OAuthHttpClient(BaseHttpClient):
     async def get_token(self, url: str, params: dict) -> dict:
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
-        response = await self.post(url, data=params, headers=headers)
-        
-        if response.get('content_type') == 'text/html':
-            html_content = await response.text()
+        try:
+            response = await self.post(url, data=params, headers=headers)
+            return response
+        except ContentTypeError as e:
+      
+            html_content = await e.response.text()
+            self.logger.error("ContentTypeError: %s", str(e))
             self.logger.error("HTML Response: %s", html_content)
             
-        return response
+            raise 
 
     async def get_user_info(self, url: str, token: str) -> dict:
         headers = {"Authorization": f"Bearer {token}"}
