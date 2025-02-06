ce40ce27312ac0c7cd0e6d26506ed66ea4ee625a
mikey-semy <toshkin.mikhail@yandex.ru>
2025-02-06 11:53:59 +0000

    fix: закрытие сессии aiohttp

--- app/core/http/base.py
+++ app/core/http/base.py
@@ -13,27 +13,37 @@ class BaseHttpClient:
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
+
+    async def close(self):
         if self._session:
             await self._session.close()
             self._session = None
-
+            self.logger.debug("Сессия закрыта")
+            
     async def _get_session(self) -> aiohttp.ClientSession:
         if not self._session:
             self._session = aiohttp.ClientSession()
         return self._session
 
     async def get(self, url: str, headers: dict = None) -> dict:
-        session = await self._get_session()
-        self.logger.debug("GET запрос к %s", url)
-        async with session.get(url, headers=headers) as resp:
-            return await resp.json()
+        try:
+            session = await self._get_session()
+            self.logger.debug("GET запрос к %s", url)
+            async with session.get(url, headers=headers) as resp:
+                return await resp.json()
+        finally:
+            await self.close()
 
     async def post(self, url: str, data: dict = None, headers: dict = None) -> dict:
-        if data:
-            # Фильтруем None значения из параметров
-            data = {k: v for k, v in data.items() if v is not None}
+        try:
+            if data:
+                # Фильтруем None значения из параметров
+                data = {k: v for k, v in data.items() if v is not None}
 
-        session = await self._get_session()
-        self.logger.debug("POST запрос к %s с данными %s", url, data)
-        async with session.post(url, data=data, headers=headers) as resp:
-            return await resp.json()
+            session = await self._get_session()
+            self.logger.debug("POST запрос к %s с данными %s", url, data)
+            async with session.post(url, data=data, headers=headers) as resp:
+                return await resp.json()
+        finally:
+            await self.close()
