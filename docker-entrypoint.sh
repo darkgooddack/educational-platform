2590875a3d1ef155802780c36c3a0874db007da8
MikhailTo <toshkin.mikhail@yandex.ru>
2025-02-16 19:22:50 +0300

    fix: создание файла логов с правами

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
+
+echo "Создание директории для логов и файла логов"
+mkdir -p /monitoring/logs
+touch /monitoring/logs/app.log
+chmod 666 /monitoring/logs/app.log
+
 echo "Запуск сервиса"
 uv run start
