8be8b2f006f1c8a07d734beb9275e90076f39481
MikhailTo <toshkin.mikhail@yandex.ru>
2025-02-16 13:31:28 +0300

    style: подготовка к слиянию с demo с новыми логами

--- app/core/config/app.py
+++ app/core/config/app.py
@@ -96,7 +96,7 @@ class LogConfig:
 
     LOG_FORMAT = "pretty" # Используется log_format из settings
     LEVEL = "DEBUG"
-    FILE = "logs/app.log"
+    FILE = "monitoring/logs/app.log"
     MAX_BYTES = 10485760  # 10MB
     BACKUP_COUNT = 5
     ENCODING = "utf-8"
--- loki/loki-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p /tmp/loki/chunks /tmp/loki/index /tmp/loki/cache
-chown -R loki:loki /tmp/loki
-exec /usr/bin/loki -config.file=/etc/loki/local-config.yaml
