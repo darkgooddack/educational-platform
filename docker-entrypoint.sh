115e178b8ef14944a24eb1234ca4973d35f9fca7
mikey-semy <toshkin.mikhail@yandex.ru>
2025-02-17 04:33:39 +0000

    fix: импорт os и на всякий случай еще одно создание директории с файлом логов

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
 
-echo "Создание директории для логов и файла логов"
-mkdir -p /monitoring/logs
-touch /monitoring/logs/app.log
-chmod 666 /monitoring/logs/app.log
+set -e
+
+echo "Создаём директорию для логов"
+mkdir -p /monitoring/logs 2>&1 || echo "Не удалось создать директорию: $?"
+ls -la /monitoring/logs 2>&1 || echo "Не удалось получить список файлов: $?"
+
+echo "Создаём файл логов и выставляем права" 
+touch /monitoring/logs/app.log 2>&1 || echo "Не удалось создать файл логов: $?"
+chmod 666 /monitoring/logs/app.log 2>&1 || echo "Не удалось установить права: $?"
+
 
 echo "Запуск сервиса"
 uv run start
