751cf0a74629bbacbab4b1dd37c038f178653b2c
mikey-semy <toshkin.mikhail@yandex.ru>
2025-02-18 07:04:04 +0000

    fix: циклические импорты

--- app/core/middlewares/auth.py
+++ app/core/middlewares/auth.py
@@ -1,13 +1,16 @@
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
-from app.core.storages.redis.auth import AuthRedisStorage
+
 
 class LastActivityMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        from app.core.storages.redis.auth import AuthRedisStorage
+        
         # Получаем токен из заголовка
         auth_header = request.headers.get("Authorization", "")
         if auth_header.startswith("Bearer "):
             token = auth_header.split(" ")[1]
+            
             # Обновляем время последней активности
             redis = AuthRedisStorage()
             await redis.update_last_activity(token)
